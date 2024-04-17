8e10d5681eb174cf3f513b731d238161b3984ee2
Panus Chachon <dev.pnz7@gmail.com>
2024-04-17 12:14:47 +0700

    Update 4.4

--- js/gall.js
+++ js/gall.js
@@ -1,34 +1,34 @@
 
 
 
-function setDynamicZoom() {
+// function setDynamicZoom() {
    
-    const screenWidth = window.screen.width;
-    console.log('screenWidth',screenWidth); 
+//     const screenWidth = window.screen.width;
+//     console.log('screenWidth',screenWidth); 
     
-    const zoomThresholds = [1680,1440,1280,1110];
-    const zoomFactors = ['60%','70%','80%','90%'];
+//     const zoomThresholds = [1680,1440,1280,1110];
+//     const zoomFactors = ['60%','70%','80%','90%'];
    
-    let zoomFactor = '100%';
-    for (let i = 0; i < zoomThresholds.length; i++) {
-      if (screenWidth < zoomThresholds[i]) {
-        zoomFactor = zoomFactors[i];
-        console.log('zoomFactor',zoomFactor); 
-        break;
-      }
-    }
+//     let zoomFactor = '100%';
+//     for (let i = 0; i < zoomThresholds.length; i++) {
+//       if (screenWidth < zoomThresholds[i]) {
+//         zoomFactor = zoomFactors[i];
+//         console.log('zoomFactor',zoomFactor); 
+//         break;
+//       }
+//     }
   
-    elmtree_gall.style.zoom =  zoomFactor
-    elmcountry_trunk.style.zoom =  zoomFactor
+//     elmtree_gall.style.zoom =  zoomFactor
+//     elmcountry_trunk.style.zoom =  zoomFactor
    
-    if(screenWidth < 500)
-    {
-      document.body.style.zoom = zoomFactor;
-    } 
-  }
+//     if(screenWidth < 500)
+//     {
+//       document.body.style.zoom = zoomFactor;
+//     } 
+//   }
    
-window.addEventListener("load", setDynamicZoom);
-window.addEventListener("resize", setDynamicZoom)
+// window.addEventListener("load", setDynamicZoom);
+// window.addEventListener("resize", setDynamicZoom)
 
 
 const modalImage = document.getElementById('modal-image'); 
