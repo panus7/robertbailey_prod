3ff00ac2f8f5284fed2541d38beae4a46d186d3c
panus7 <dev.pnz7@gmail.com>
2024-06-11 22:47:46 +0700

    chang font size js error

--- js/home.js
+++ js/home.js
@@ -2,34 +2,38 @@ const cookieDialog = document.getElementById('cookie-dialog');
 const acceptCookieBtn = document.getElementById('accept-cookie-btn');
 const elm_p_robbgpre01_size = document.getElementById("div_robbgpre01_size");
 
-acceptCookieBtn.addEventListener('click', function() {
-  
-  cookieDialog.style.display = 'none'; 
+if (acceptCookieBtn) {
+  acceptCookieBtn.addEventListener('click', function () {
 
-  localStorage.setItem('acceptCookie', true);
+    cookieDialog.style.display = 'none';
 
-  // Alternative: Using cookies (less secure, might require additional setup)
-  // document.cookie = "acceptCookie=true; expires=Thu, 31 Dec 2024 23:59:59 GMT; path=/";
+    localStorage.setItem('acceptCookie', true);
 
-});
- 
-if (localStorage.getItem('acceptCookie')) {
-  cookieDialog.style.display = 'none';
+    // Alternative: Using cookies (less secure, might require additional setup)
+    // document.cookie = "acceptCookie=true; expires=Thu, 31 Dec 2024 23:59:59 GMT; path=/";
+
+  });
 }
- 
+
+
+if (localStorage.getItem('acceptCookie')) {
+  if (cookieDialog)
+    cookieDialog.style.display = 'none';
+}
+
 function setDynamicZoom() {
   const elmcountry_trunk = document.getElementById("country_trunk");
   const elmtree_gall = document.getElementById("tree_gall");
-   
+
   const screenWidth = window.screen.width;
   //console.log('screenWidth',screenWidth); 
-  
+
   // const zoomThresholds = [1680,1440,1280,111,600];
   //  const zoomFactors = ['40%','60%','70%','80%','90%'];
 
-  const zoomThresholds = [600,800,1000];
-  const zoomFactors = ['40%','80%','90%'];
- 
+  const zoomThresholds = [600, 800, 1000];
+  const zoomFactors = ['40%', '80%', '90%'];
+
   let zoomFactor = '100%';
   for (let i = 0; i < zoomThresholds.length; i++) {
     if (screenWidth < zoomThresholds[i]) {
@@ -39,19 +43,22 @@ function setDynamicZoom() {
     }
   }
 
-  elmtree_gall.style.zoom =  zoomFactor
-  elmcountry_trunk.style.zoom =  zoomFactor 
+  if (elmtree_gall)
+    elmtree_gall.style.zoom = zoomFactor
+
+  if (elmcountry_trunk)
+    elmcountry_trunk.style.zoom = zoomFactor
 
 
 
-  var elm_div_robbgpre01 = document.getElementById("div_robbgpre01");  
-  var rect = elm_div_robbgpre01.getBoundingClientRect();
-  var width = rect.width;
-  var height = rect.height;
+  // var elm_div_robbgpre01 = document.getElementById("div_robbgpre01");
+  // var rect = elm_div_robbgpre01.getBoundingClientRect();
+  // var width = rect.width;
+  // var height = rect.height;
   // elm_p_robbgpre01_size.textContent = "div_robbgpre01> (Width: " + width + " px, Height:" + height + " px )";
 
 }
- 
+
 window.addEventListener("load", setDynamicZoom);
 window.addEventListener("resize", setDynamicZoom)
 
--- js/zoom.js
+++ js/zoom.js
@@ -1,51 +1,56 @@
 function checkResolutionAndZoom() {
-    const screenWidth = window.screen.width;
-    console.log('Width',screenWidth);  
-    //const zoomThresholds = [1680,1440,1280,1110];
-    //const zoomFactors = ['70%','80%','90%','100%']; 
-    const zoomThresholds = [1000,1200,1400,1600];
-    const zoomFactors = ['70%','80%','90%','100%']; 
-    let zoomFactor = '100%';
-    for (let i = 0; i < zoomThresholds.length; i++) {
-      if (screenWidth <= zoomThresholds[i]) {
-        zoomFactor = zoomFactors[i];
-        console.log('main zoomFactor',zoomFactor) 
-        break;
-      }
-    } 
-    document.body.style.zoom = zoomFactor; 
-
-    if(screenWidth > 800 && screenWidth < 1300)
-    {
-      const navItems = document.getElementsByClassName('nav-link');  
-      for (let i = 0; i < navItems.length; i++) {
-        navItems[i].style.fontSize = '14px';
-      }
-    }  
-
-    //footer_card
-
-  } 
-  window.addEventListener("load", checkResolutionAndZoom);
+  const screenWidth = window.screen.width;
+  console.log('Width', screenWidth);
+  //const zoomThresholds = [1680,1440,1280,1110];
+  //const zoomFactors = ['70%','80%','90%','100%']; 
 
 
-  function setfooterDynamicZoom() {
+  //2024-06-11
+  // const zoomThresholds = [1000,1200,1400,1600];
+  // const zoomFactors = ['70%','80%','90%','100%']; 
 
-    const elm_footer_card = document.getElementById("footer_card");      
-    const screenWidth = window.screen.width; 
-    const zoomThresholds = [375,600,800,1000];
-    const zoomFactors = ['80%','90%','80%','90%'];
-   
-    let zoomFactor = '100%';
-    for (let i = 0; i < zoomThresholds.length; i++) {
-      if (screenWidth < zoomThresholds[i]) {
-        zoomFactor = zoomFactors[i];         
-        break;
-      }
-    } 
-    elm_footer_card.style.zoom =  zoomFactor  
+  const zoomThresholds = [900, 1000, 1200, 1400, 1600];
+  const zoomFactors = ['60%', '70%', '80%', '90%', '100%'];
+
+  let zoomFactor = '100%';
+  for (let i = 0; i < zoomThresholds.length; i++) {
+    if (screenWidth <= zoomThresholds[i]) {
+      zoomFactor = zoomFactors[i];
+      console.log('main zoomFactor', zoomFactor)
+      break;
+    }
   }
-   
-  window.addEventListener("load", setfooterDynamicZoom);
-  window.addEventListener("resize", setfooterDynamicZoom)
-  
+  document.body.style.zoom = zoomFactor;
+
+  if (screenWidth > 800 && screenWidth < 1300) {
+    const navItems = document.getElementsByClassName('nav-link');
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].style.fontSize = '14px';
+    }
+  }
+
+  //footer_card
+
+}
+window.addEventListener("load", checkResolutionAndZoom);
+
+
+function setfooterDynamicZoom() {
+
+  const elm_footer_card = document.getElementById("footer_card");
+  const screenWidth = window.screen.width;
+  const zoomThresholds = [375, 600, 800, 1000];
+  const zoomFactors = ['80%', '90%', '80%', '90%'];
+
+  let zoomFactor = '100%';
+  for (let i = 0; i < zoomThresholds.length; i++) {
+    if (screenWidth < zoomThresholds[i]) {
+      zoomFactor = zoomFactors[i];
+      break;
+    }
+  }
+  elm_footer_card.style.zoom = zoomFactor
+}
+
+window.addEventListener("load", setfooterDynamicZoom);
+window.addEventListener("resize", setfooterDynamicZoom)
