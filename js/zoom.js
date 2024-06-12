b2d0c861eda01f8967ae2975bfa6b3c00db74b3d
panus7 <dev.pnz7@gmail.com>
2024-06-12 22:53:53 +0700

    update web

--- js/zoom.js
+++ js/zoom.js
@@ -8,26 +8,41 @@ function checkResolutionAndZoom() {
   //2024-06-11
   // const zoomThresholds = [1000,1200,1400,1600];
   // const zoomFactors = ['70%','80%','90%','100%']; 
+  const elm_idzoom = document.getElementById("idzoom");
 
-  const zoomThresholds = [400, 820, 900, 1200, 1600];
-  const zoomFactors = ['60%', '70%', '80%', '90%', '100%'];
+  if (/iPhone/.test(navigator.userAgent)) {
+    // Set the initial zoom level based on the screen width
+    const screenWidth = screen.width;
+    const zoomLevel = screenWidth / 375;
+    document.documentElement.style.zoom = zoomLevel;
+    elm_idzoom.textContent = 'iphone zoom level : ' + zoomLevel + ' screen ' + screenWidth;
+  }
+  else {
 
-  let zoomFactor = '100%';
-  for (let i = 0; i < zoomThresholds.length; i++) {
-    if (screenWidth <= zoomThresholds[i]) {
-      zoomFactor = zoomFactors[i];
-      console.log('main zoomFactor', zoomFactor)
-      break;
+
+    const zoomThresholds = [400, 820, 900, 1200, 1600];
+    const zoomFactors = ['100%', '70%', '80%', '90%', '100%'];
+
+    let zoomFactor = '100%';
+    for (let i = 0; i < zoomThresholds.length; i++) {
+      if (screenWidth <= zoomThresholds[i]) {
+        zoomFactor = zoomFactors[i];
+        console.log('main zoomFactor', zoomFactor)
+        break;
+      }
+    }
+    document.body.style.zoom = zoomFactor;
+    elm_idzoom.textContent = 'zoom : ' + zoomFactor + ' screen ' + screenWidth;
+
+    if (screenWidth > 800 && screenWidth < 1300) {
+      const navItems = document.getElementsByClassName('nav-link');
+      for (let i = 0; i < navItems.length; i++) {
+        navItems[i].style.fontSize = '14px';
+      }
     }
   }
-  document.body.style.zoom = zoomFactor;
 
-  if (screenWidth > 800 && screenWidth < 1300) {
-    const navItems = document.getElementsByClassName('nav-link');
-    for (let i = 0; i < navItems.length; i++) {
-      navItems[i].style.fontSize = '14px';
-    }
-  }
+
 
   //footer_card
 
@@ -35,7 +50,12 @@ function checkResolutionAndZoom() {
 window.addEventListener("load", checkResolutionAndZoom);
 
 
+
+
 function setfooterDynamicZoom() {
+
+
+
 
   const elm_footer_card = document.getElementById("footer_card");
   const screenWidth = window.screen.width;
@@ -50,6 +70,9 @@ function setfooterDynamicZoom() {
     }
   }
   elm_footer_card.style.zoom = zoomFactor
+
+
+
 }
 
 window.addEventListener("load", setfooterDynamicZoom);
