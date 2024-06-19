e365620679c1f7af8906c443d561ba1045b22e96
panus7 <dev.pnz7@gmail.com>
2024-06-19 08:36:05 +0700

    set bg nav toggle

--- js/navbarcolor.js
+++ js/navbarcolor.js
@@ -1,5 +1,12 @@
 // Change navbar background color on scroll
+// 200orinigal 200 px
 $(window).scroll(function () {
     $('nav').toggleClass('bg-lime',
- $(this).scrollTop() > 200)
+        $(this).scrollTop() > 10)
 })
+
+// Mobile Clivk Menu Set Blur Bg
+$('.navbar-toggler').click(function () {
+    $('nav').toggleClass('bg-lime');
+});
+
