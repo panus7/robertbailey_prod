cf740279770cb086d4a17d1a007944d3b3538e5e
heptragon <poraphat708@gmail.com>
2024-07-12 00:40:30 +0700

    update main menu

--- js/home.js
+++ js/home.js
@@ -21,50 +21,50 @@ if (localStorage.getItem('acceptCookie')) {
     cookieDialog.style.display = 'none';
 }
 
-function setDynamicZoom() {
-  const elmcountry_trunk = document.getElementById("country_trunk");
-  const elmtree_gall = document.getElementById("tree_gall");
+//function setDynamicZoom() {
+//  const elmcountry_trunk = document.getElementById("country_trunk");
+//  const elmtree_gall = document.getElementById("tree_gall");
 
-  const screenWidth = window.screen.width;
-  //console.log('screenWidth',screenWidth); 
+//  const screenWidth = window.screen.width;
+//  //console.log('screenWidth',screenWidth); 
 
-  // const zoomThresholds = [1680,1440,1280,111,600];
-  //  const zoomFactors = ['40%','60%','70%','80%','90%'];
+//  // const zoomThresholds = [1680,1440,1280,111,600];
+//  //  const zoomFactors = ['40%','60%','70%','80%','90%'];
 
-  //orignial
-  // const zoomThresholds = [600, 800, 1000];
-  // const zoomFactors = ['40%', '80%', '90%'];
+//  //orignial
+//  // const zoomThresholds = [600, 800, 1000];
+//  // const zoomFactors = ['40%', '80%', '90%'];
 
-  const zoomThresholds = [600, 800, 1500];
-  const zoomFactors = ['40%', '60%', '80%'];
+//  const zoomThresholds = [600, 800, 1500];
+//  const zoomFactors = ['40%', '60%', '80%'];
 
-  let zoomFactor = '100%';
-  for (let i = 0; i < zoomThresholds.length; i++) {
-    if (screenWidth < zoomThresholds[i]) {
-      zoomFactor = zoomFactors[i];
-      // console.log('zoom tree_gall ',zoomFactor); 
-      break;
-    }
-  }
+//  let zoomFactor = '100%';
+//  for (let i = 0; i < zoomThresholds.length; i++) {
+//    if (screenWidth < zoomThresholds[i]) {
+//      zoomFactor = zoomFactors[i];
+//      // console.log('zoom tree_gall ',zoomFactor); 
+//      break;
+//    }
+//  }
 
-  if (elmtree_gall)
-    elmtree_gall.style.zoom = zoomFactor
+//  if (elmtree_gall)
+//    elmtree_gall.style.zoom = zoomFactor
 
-  if (elmcountry_trunk)
-    elmcountry_trunk.style.zoom = zoomFactor
+//  if (elmcountry_trunk)
+//    elmcountry_trunk.style.zoom = zoomFactor
 
 
 
-  // var elm_div_robbgpre01 = document.getElementById("div_robbgpre01");
-  // var rect = elm_div_robbgpre01.getBoundingClientRect();
-  // var width = rect.width;
-  // var height = rect.height;
-  // elm_p_robbgpre01_size.textContent = "div_robbgpre01> (Width: " + width + " px, Height:" + height + " px )";
+//  // var elm_div_robbgpre01 = document.getElementById("div_robbgpre01");
+//  // var rect = elm_div_robbgpre01.getBoundingClientRect();
+//  // var width = rect.width;
+//  // var height = rect.height;
+//  // elm_p_robbgpre01_size.textContent = "div_robbgpre01> (Width: " + width + " px, Height:" + height + " px )";
 
-}
+//}
 
-window.addEventListener("load", setDynamicZoom);
-window.addEventListener("resize", setDynamicZoom)
+//window.addEventListener("load", setDynamicZoom);
+//window.addEventListener("resize", setDynamicZoom)
 
 
 // function isIOS() {
