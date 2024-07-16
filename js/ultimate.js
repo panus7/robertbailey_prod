38621e342a2fc1c412613258ba68e4004726f0ce
Panus Chachon <dev.pnz7@gmail.com>
2024-07-16 22:29:14 +0700

    update edit

--- js/ultimate.js
+++ js/ultimate.js
@@ -1,14 +1,10 @@
 
 function setDynamicZoom() {
+ 
     const elmcountry_trunk = document.getElementById("country_trunk");
     const elmtree_gall = document.getElementById("tree_gall");
 
     const screenWidth = window.screen.width;
-    //console.log('screenWidth',screenWidth); 
-
-    // const zoomThresholds = [1680,1440,1280,111,600];
-    //  const zoomFactors = ['40%','60%','70%','80%','90%'];
-
     const zoomThresholds = [600, 800, 1000];
     const zoomFactors = ['40%', '80%', '90%'];
 
@@ -27,14 +23,11 @@ function setDynamicZoom() {
     if (elmcountry_trunk)
         elmcountry_trunk.style.zoom = zoomFactor
 
-
-
     // var elm_div_robbgpre01 = document.getElementById("div_robbgpre01");
     // var rect = elm_div_robbgpre01.getBoundingClientRect();
     // var width = rect.width;
     // var height = rect.height;
     // elm_p_robbgpre01_size.textContent = "div_robbgpre01> (Width: " + width + " px, Height:" + height + " px )";
-
 }
 
 window.addEventListener("load", setDynamicZoom);
