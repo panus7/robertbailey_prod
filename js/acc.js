abd2228eceb031a14ccc07e5dd9de70c650435b4
Panus Chachon <dev.pnz7@gmail.com>
2024-07-22 17:50:09 +0700

    update acc

--- js/acc.js
+++ js/acc.js
@@ -1,13 +1,13 @@
-function adjustBackgroundImage() {
-    const screenWidth = window.innerWidth;
-    const mobileBreakpoint = 768; // Adjust as needed
-    const element = document.getElementById('a1');
-   //robbgpreA01
-     if (screenWidth <= mobileBreakpoint) {
-        element.style.backgroundImage = "url('../img/rob_accessories/BG_Accessories_page_BG1.jpg'"
-        element.style.backgroundSize="200% 70%";
-     }
-  }
+// function adjustBackgroundImage() {
+//     const screenWidth = window.innerWidth;
+//     const mobileBreakpoint = 768; // Adjust as needed
+//     const element = document.getElementById('a1');
+//    //robbgpreA01
+//      if (screenWidth <= mobileBreakpoint) {
+//         element.style.backgroundImage = "url('../img/rob_accessories/BG_Accessories_page_BG1.jpg'"
+//         element.style.backgroundSize="200% 70%";
+//      }
+//   }
   
-  window.addEventListener('load', adjustBackgroundImage);
-  window.addEventListener('resize', adjustBackgroundImage);
+//   window.addEventListener('load', adjustBackgroundImage);
+//   window.addEventListener('resize', adjustBackgroundImage);
