aae24376b7bdfd9a328107076c1ca31a538c2413
Panus Chachon <dev.pnz7@gmail.com>
2024-09-27 15:18:34 +0700

    update nav

--- js/navbarcolor.js
+++ js/navbarcolor.js
@@ -2,7 +2,7 @@
 // 200orinigal 200 px
 $(window).scroll(function () {
     $('nav').toggleClass('bg-lime',
-        $(this).scrollTop() > 10)
+        $(this).scrollTop() > 1)
 })
 
 // Mobile Clivk Menu Set Blur Bg
@@ -10,3 +10,7 @@ $('.navbar-toggler').click(function () {
     $('nav').toggleClass('bg-lime');
 });
 
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => { 
+    $('nav').toggleClass('bg-lime');
+});
