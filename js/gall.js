677ca445d3fa20b92df36f23d70fca98ca51fdea
Panus Chachon <dev.pnz7@gmail.com>
2024-11-19 13:56:59 +0700

    Update Gall Link Open from Mobile

--- js/gall.js
+++ js/gall.js
@@ -1,5 +1,17 @@
 
+function preventMobileNewTab() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+    if (isMobile) {
+      const links = document.querySelectorAll('a[target="_blank"]');
+      links.forEach(link => {
+        link.removeAttribute('target');
+      });
+    }
+  }
 
+  // Call the function to check and modify links
+preventMobileNewTab();
 
 // function setDynamicZoom() {
    
@@ -31,58 +43,58 @@
 // window.addEventListener("resize", setDynamicZoom)
 
 
-const modalImage = document.getElementById('modal-image'); 
+// const modalImage = document.getElementById('modal-image'); 
 
-const opendlg1 = document.getElementById('opendlg1');   
-if(null != opendlg1)
-opendlg1.addEventListener('click', (event) => handleClick(event, opendlg1.getAttribute('data-image-path')));
+// const opendlg1 = document.getElementById('opendlg1');   
+// if(null != opendlg1)
+// opendlg1.addEventListener('click', (event) => handleClick(event, opendlg1.getAttribute('data-image-path')));
 
-const opendlg2 = document.getElementById('opendlg2');   
-if(null != opendlg2)
-opendlg2.addEventListener('click', (event) => handleClick(event, opendlg2.getAttribute('data-image-path')));
+// const opendlg2 = document.getElementById('opendlg2');   
+// if(null != opendlg2)
+// opendlg2.addEventListener('click', (event) => handleClick(event, opendlg2.getAttribute('data-image-path')));
 
-const opendlg3 = document.getElementById('opendlg3');   
-if(null != opendlg3)
-opendlg3.addEventListener('click', (event) => handleClick(event, opendlg3.getAttribute('data-image-path')));
+// const opendlg3 = document.getElementById('opendlg3');   
+// if(null != opendlg3)
+// opendlg3.addEventListener('click', (event) => handleClick(event, opendlg3.getAttribute('data-image-path')));
 
-const opendlg4 = document.getElementById('opendlg4');   
-if(null != opendlg4)
-opendlg4.addEventListener('click', (event) => handleClick(event, opendlg4.getAttribute('data-image-path')));
+// const opendlg4 = document.getElementById('opendlg4');   
+// if(null != opendlg4)
+// opendlg4.addEventListener('click', (event) => handleClick(event, opendlg4.getAttribute('data-image-path')));
 
-const opendlg5 = document.getElementById('opendlg5');   
-if(null != opendlg5)
-opendlg5.addEventListener('click', (event) => handleClick(event, opendlg5.getAttribute('data-image-path')));
+// const opendlg5 = document.getElementById('opendlg5');   
+// if(null != opendlg5)
+// opendlg5.addEventListener('click', (event) => handleClick(event, opendlg5.getAttribute('data-image-path')));
 
-const opendlg6 = document.getElementById('opendlg6');   
-if(null != opendlg6)
-opendlg6.addEventListener('click', (event) => handleClick(event, opendlg6.getAttribute('data-image-path')));
+// const opendlg6 = document.getElementById('opendlg6');   
+// if(null != opendlg6)
+// opendlg6.addEventListener('click', (event) => handleClick(event, opendlg6.getAttribute('data-image-path')));
 
-const opendlg7 = document.getElementById('opendlg7');   
-if(null != opendlg7)
-opendlg7.addEventListener('click', (event) => handleClick(event, opendlg7.getAttribute('data-image-path')));
+// const opendlg7 = document.getElementById('opendlg7');   
+// if(null != opendlg7)
+// opendlg7.addEventListener('click', (event) => handleClick(event, opendlg7.getAttribute('data-image-path')));
 
-const opendlg8 = document.getElementById('opendlg8');   
-if(null != opendlg8)
-opendlg8.addEventListener('click', (event) => handleClick(event, opendlg8.getAttribute('data-image-path')));
+// const opendlg8 = document.getElementById('opendlg8');   
+// if(null != opendlg8)
+// opendlg8.addEventListener('click', (event) => handleClick(event, opendlg8.getAttribute('data-image-path')));
 
-const opendlg9 = document.getElementById('opendlg9');   
-if(null != opendlg9)
-opendlg9.addEventListener('click', (event) => handleClick(event, opendlg9.getAttribute('data-image-path')));
+// const opendlg9 = document.getElementById('opendlg9');   
+// if(null != opendlg9)
+// opendlg9.addEventListener('click', (event) => handleClick(event, opendlg9.getAttribute('data-image-path')));
 
-const opendlg10 = document.getElementById('opendlg10');   
-if(null != opendlg10)
-opendlg10.addEventListener('click', (event) => handleClick(event, opendlg10.getAttribute('data-image-path')));
+// const opendlg10 = document.getElementById('opendlg10');   
+// if(null != opendlg10)
+// opendlg10.addEventListener('click', (event) => handleClick(event, opendlg10.getAttribute('data-image-path')));
 
-const opendlg11 = document.getElementById('opendlg11');   
-if(null != opendlg11)
-opendlg11.addEventListener('click', (event) => handleClick(event, opendlg11.getAttribute('data-image-path')));
+// const opendlg11 = document.getElementById('opendlg11');   
+// if(null != opendlg11)
+// opendlg11.addEventListener('click', (event) => handleClick(event, opendlg11.getAttribute('data-image-path')));
 
-const opendlg12 = document.getElementById('opendlg12');   
-if(null != opendlg12)
-opendlg12.addEventListener('click', (event) => handleClick(event, opendlg12.getAttribute('data-image-path')));
+// const opendlg12 = document.getElementById('opendlg12');   
+// if(null != opendlg12)
+// opendlg12.addEventListener('click', (event) => handleClick(event, opendlg12.getAttribute('data-image-path')));
 
-function handleClick(event,imagePath) { 
-    modalImage.src = imagePath; 
-    event.preventDefault();
-}
+// function handleClick(event,imagePath) { 
+//     modalImage.src = imagePath; 
+//     event.preventDefault();
+// }
  
